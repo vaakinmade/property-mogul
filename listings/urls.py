40abe528af21b6eabfd3160a6e024c6e77b19b58
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-19 19:50:03 +0100

    Refactored as view no longer inherits from ListView

--- listings/urls.py
+++ listings/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^search$', views.SearchMogul.as_view(), name='search'),
+    url(r'^search$', views.SearchMogulView.as_view(), name='search'),
 ]
