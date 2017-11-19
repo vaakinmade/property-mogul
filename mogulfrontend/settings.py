bfe14ef8f862060c53f0a8490c9de0c3f9ecaf3c
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-19 06:28:46 +0100

    added staticfiles config

--- mogulfrontend/settings.py
+++ mogulfrontend/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mogulfrontend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_URL = os.environ.get('STATIC_URL', STATIC_URL)
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
