d566f32de79389ac8a4058f52c5c3e9575b59a12
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-19 19:51:37 +0100

    COmpleted Search View with template rendering.

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,19 @@
 from django.views import generic
+from django.shortcuts import render
 
-from service.mogul_api.interface import MogulAPI
+from service.mogul_api import interface
 
 
-class SearchMogul(generic.ListView):
-	context_object_name = "search_items"
-	template_name = "listing/search_result.html"
+class SearchMogulView(generic.View):
 
 	def get(self, request):
-		books_list = services.get_books('2009', 'edwards')
-		return render(request,'books.html',books_list)
+		query_dict = self.request.GET
+
+		if query_dict:
+			query = (query_dict.dict())
+
+			response = interface.search_listings(**query)
+			listings = {"listings" : response.json()}
+			return render(request, 'listings/search_result.html', listings)
+		else:
+			return render(request, 'listings/search_result.html')
