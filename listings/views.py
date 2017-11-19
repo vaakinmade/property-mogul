72415e88791c02841c4df22a8a72cfdf960a9503
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-19 15:06:36 +0100

    Search view for rendering search listings results

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from service.mogul_api.interface import MogulAPI
+
+
+class SearchMogul(generic.ListView):
+	context_object_name = "search_items"
+	template_name = "listing/search_result.html"
+
+	def get(self, request):
+		books_list = services.get_books('2009', 'edwards')
+		return render(request,'books.html',books_list)
