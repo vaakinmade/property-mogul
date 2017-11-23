f20da9f28437df0004196842df3d8d2d4cb290c4
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-23 06:33:42 +0100

    New property detail generic view.

--- listings/views.py
+++ listings/views.py
@@ -3,21 +3,36 @@ from django.shortcuts import render
 
 from service.mogul_api import interface
 
+import json
+from iso8601 import parse_date
+
 
 class SearchMogulView(generic.View):
+	@staticmethod
+	def convert_to_datetime(response):
+		listing = list()
+		for each in response.json():
+			each.update({"created_at":parse_date(each['created_at'])})
+			listing.append(each)
+		return listing
+
 
 	def get(self, request):
 		query_dict = self.request.GET
 
 		if query_dict:
 			query = (query_dict.dict())
-
 			response = interface.search_listings(**query)
-			listings = {"listings" : response.json()}
+
+			listings = {"listings" : SearchMogulView.convert_to_datetime(response)}
 			return render(request, 'listings/search_result.html', listings)
 		else:
 			return render(request, 'listings/search_result.html')
 
 
-#class ListingListView(generic.ListView):
+class ListingDetailView(generic.View):
 
+	def get(self, request, pk):
+		response = interface.detail_listings(self.kwargs.get('pk'))
+		listing = {"listing": response.json()}
+		return render(request, 'listings/listing_detail.html', listing)
