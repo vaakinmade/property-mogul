e4b52f771fd742c9ea905ee529d27650a999b0a4
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-23 06:30:26 +0100

    New mogul_api interface method for retrieving property details

--- service/mogul_api/interface.py
+++ service/mogul_api/interface.py
@@ -8,3 +8,9 @@ def search_listings(**kwargs):
 	response = requests.get(url, params=kwargs)
 	print("result", response.status_code)
 	return response
+
+def detail_listings(listing_pk):
+	url = MOGUL_API_BASE_HOST + '/api/v1/listings/' + listing_pk
+	response = requests.get(url)
+	print("result", response.status_code)
+	return response
