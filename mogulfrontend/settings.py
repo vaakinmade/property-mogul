3dde7e0273108a487274527e21e45428fb304387
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-24 17:15:06 +0100

    turned on AWS S3 settings

--- mogulfrontend/settings.py
+++ mogulfrontend/settings.py
@@ -76,7 +76,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mogulfrontend.wsgi.application'
 
 # AWS config
-if 'DYNO' not in os.environ:
+if 'DYNO' in os.environ:
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME', cast=str)
     AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', cast=str)
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', cast=str)
