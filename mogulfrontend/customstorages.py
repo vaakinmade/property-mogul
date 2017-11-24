48ef0e81efe5f9a5a294a888421caeccb1239858
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-24 15:45:14 +0100

    storage class for AWS remote static files access

---
+++ mogulfrontend/customstorages.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from decouple import config
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class StaticStorage(S3Boto3Storage):
+    location = settings.STATICFILES_LOCATION
+    host = "s3.{}.amazonaws.com".format(config('AWS_REGION'))
