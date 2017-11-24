b3c312c1af17fbbc4b4b27087a0e53de478d4cd9
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-24 23:26:09 +0100

    Added new url routing for sale properties

--- listings/urls.py
+++ listings/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^search$', views.SearchMogulView.as_view(), name='search'),
     url(r'(?P<pk>\d+)$', views.ListingDetailView.as_view(), name='detail'),
     url(r'^for-rent$', views.PropertyRentView.as_view(), name='rent'),
+    url(r'^for-sale$', views.PropertySaleView.as_view(), name='sale'),
 ]
