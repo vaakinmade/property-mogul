92e48e09355f75cb054330415ffa334c047991f9
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-24 21:39:14 +0100

    Added new mogul-api endpoint

--- service/mogul_api/interface.py
+++ service/mogul_api/interface.py
@@ -3,6 +3,11 @@ import requests
 from mogulfrontend.settings import MOGUL_API_BASE_HOST
 
 
+def get_rent_listings():
+	url = MOGUL_API_BASE_HOST + '/api/v1/listings/rent'
+	response = requests.get(url)
+	return response
+
 def search_listings(**kwargs):
 	url = MOGUL_API_BASE_HOST + '/api/v1/listings/search'
 	response = requests.get(url, params=kwargs)
