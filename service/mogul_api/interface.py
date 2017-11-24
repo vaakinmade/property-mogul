e1f166caf603cac03b015114d98a9c3fb4b2f46b
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-24 15:18:34 +0100

    removed response status code prints

--- service/mogul_api/interface.py
+++ service/mogul_api/interface.py
@@ -6,11 +6,9 @@ from mogulfrontend.settings import MOGUL_API_BASE_HOST
 def search_listings(**kwargs):
 	url = MOGUL_API_BASE_HOST + '/api/v1/listings/search'
 	response = requests.get(url, params=kwargs)
-	print("result", response.status_code)
 	return response
 
 def detail_listings(listing_pk):
 	url = MOGUL_API_BASE_HOST + '/api/v1/listings/' + listing_pk
 	response = requests.get(url)
-	print("result", response.status_code)
 	return response
